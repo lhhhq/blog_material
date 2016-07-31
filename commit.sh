949d141c151a69ff918d8a0447a5e2fe5b167357
lhhhq <i6kq1995@gmail.com>
2016-07-31 18:37:13 +0800

    add commit.sh

---
+++ commit.sh
@@ -0,0 +1,19 @@
+hexo generate
+
+echo ">>>>>>What is your commit message to blog-material repo?"
+read COMMIT
+git add --all
+git commit -m "$COMMIT"
+echo "commited"
+git push
+echo "pushed all to blog-material repo"
+
+cd public
+
+git add --all
+git commit -m "$COMMIT"
+echo "commited"
+git push
+echo "pushed all to blog repo"
+
+cd ../
